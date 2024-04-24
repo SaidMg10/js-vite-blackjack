a3aec7f1af50e5c66e514ce28098673a9c169380
Said Mendoza <ticcitoby777crepis@gmail.com>
2024-04-23 20:14:47 -0500

    Correciones en imagenes

--- src/blackjack/usecases/crear-carta-html.js
+++ src/blackjack/usecases/crear-carta-html.js
@@ -7,7 +7,7 @@
 export const crearCartaHTML = ( carta ) => {
     if ( !carta ) throw new Error('La carta es un argumento obligatorio');
     document.createElement('img');
-    imgCarta.src = `assets/cartas/${ carta }.png`; //3H, JD
+    imgCarta.src = `/js-vite-blackjack/assets/cartas/${ carta }.png`; //3H, JD
     imgCarta.classList.add('carta');    
 
     return imgCarta
